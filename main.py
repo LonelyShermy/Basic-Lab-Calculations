6082f2e172c9b65b31655d9d6fbb008850230ee2
LonelyShermy <165237081+LonelyShermy@users.noreply.github.com>
2026-02-11 09:23:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import math
 ## Dumb calculator for Intermediate Lab
 def Main():
-    Data = [310, 350, 290, 300, 285] #Modify to take files
+    Data = [1000,990,1003,1000,1000,998,995,995,997,1003] #Modify to take files
     Mean = MeanFun(Data) 
     SD = StandardDeviation(Data,Mean)
     SDOM = StandardDeviationofMean(Mean, SD)
+    ErrOnMean = ErroronMean(Data, SD)
     print("Mean:", Mean)
     print("Standard Deviation", SD)
     print("SDOM", SDOM)
+    print("Error on Mean", ErrOnMean)
+
 
 def MeanFun(Data):
     return sum(Data)/len(Data)
@@ -16,9 +19,12 @@ def StandardDeviation(Data, MeanLoc):
     Tot = 0
     for i in Data:
         Tot += (i-MeanLoc)*(i-MeanLoc)
-    return math.sqrt(Tot/(MeanLoc-1))
+    return math.sqrt(Tot/(len(Data)-1))
 
 def StandardDeviationofMean(MeanLoc,SDLoc):
     return SDLoc/math.sqrt(MeanLoc)
+
+def ErroronMean(Data, SD):
+    return SD/math.sqrt(len(Data))
 
 Main()
